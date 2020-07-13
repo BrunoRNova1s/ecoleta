87acf5fff98d08116ba8252f640330c1b89662b5
Bruno Novais <bruno.br001@gmail.com>
2020-07-13 01:34:14 +0100

    points route - post

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,20 +1,49 @@
 import express from "express";
-import Knex from "./database/connection";
+import knex from "./database/connection";
+
 
 const routes = express.Router();
 
 routes.get("/items", async (request, response) => {
   //SELECT * FROM items
-  const items = await Knex('items').select('*')
+  const items = await knex('items').select('*')
 
   const serializedItems = items.map( item => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     }
   })
 
   return response.json(serializedItems)
+});
+
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  return response.json({ success: true })
+
 });
 
 export default routes
