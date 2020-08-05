398af2b7ff54f04e218a4c1159f919b9de58d4d1
Bruno Novais <bruno.br001@gmail.com>
2020-08-05 01:47:46 +0100

    UF get data

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
 
 import { FiArrowLeft } from "react-icons/fi";
@@ -7,24 +7,62 @@ import logo from "../../assets/logo.svg";
 
 import { Map, TileLayer, Marker } from "react-leaflet";
 
-import api from '../../services/api'
+import axios from "axios";
+import api from "../../services/api";
 
 interface Item {
-  id: number,
-  title: string,
-  image_url: string
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
 }
 
 const CreatePoint = () => {
-
-  const [items, setItems] = useState<Item[]>([])
+  const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState("0");
 
   useEffect(() => {
-    api.get('items').then(response => {
-      setItems(response.data)
-    })
-  }, [])
+    api.get("items").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
 
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if( selectedUf === '0') {
+      return
+    }
+
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {});
+  }, [selectedUf]);
+
+  function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+
+    setSelectedUf(uf);
+  }
 
   return (
     <div id="page-create-point">
@@ -74,8 +112,18 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">State (UF) </label>
-              <select name="uf" id="uf">
+              <select
+                name="uf"
+                id="uf"
+                value={selectedUf}
+                onChange={handleSelectedUf}
+              >
                 <option value="0">Select a state</option>
+                {ufs.map((uf) => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
               </select>
             </div>
             <div className="field">
@@ -92,14 +140,11 @@ const CreatePoint = () => {
             <span>Select one or more items below</span>
           </legend>
           <ul className="items-grid">
-            {items.map(item => (
+            {items.map((item) => (
               <li key={item.id}>
-              <img
-                src={item.image_url}
-                alt="teste"
-              />
-              <span>{item.title}</span>
-            </li>
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
             ))}
           </ul>
         </fieldset>
